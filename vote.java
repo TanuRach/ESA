c80510247ef8393c98d9830ffb4844f4c7bb5bc2
Tanu <trachanaikar@gmail.com>
2025-06-10 21:46:24 +0530

    vote check

---
+++ vote.java
@@ -0,0 +1,10 @@
+public class vote {
+    public static void main(String[] args) {
+        int age = 20; // You can replace this with user input
+        if (age >= 18) {
+            System.out.println("Eligible to vote.");
+        } else {
+            System.out.println("Not eligible to vote.");
+        }
+    }
+}
